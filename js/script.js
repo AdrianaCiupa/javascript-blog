aab4ddfb08422bf7c3a239a22f2d0dc66a6b6576
Adriana Ciupa <adriana.ciupa94@gmail.com>
2022-08-08 14:04:45 +0200

    add attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
+  console.log(event);
 
 
-  /* [DONE] remove class 'active' from all article links  */
+  /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -14,7 +16,7 @@ for(let activeLink of activeLinks){
 }
 
 
-  /* [IN PROGERSS] add class 'active' to the clicked link */
+  /* add class 'active' to the clicked link */
 
 
 clickedElement.classList.add('active');
@@ -22,7 +24,7 @@ clickedElement.classList.add('active');
 
 
 
-  /* [DONE]remove class 'active' from all articles */
+  /* remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.articles a.active');
 
@@ -33,6 +35,10 @@ clickedElement.classList.add('active');
 
 
   /* get 'href' attribute from the clicked link */
+
+  
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
