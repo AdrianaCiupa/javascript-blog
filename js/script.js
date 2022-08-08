a565ca5320bcbd8e76bc01f4b8c595aac49d7d2c
Adriana Ciupa <adriana.ciupa94@gmail.com>
2022-08-08 13:24:32 +0200

    add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,9 +13,16 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  /* [IN PROGERSS] add class 'active' to the clicked link */
+
+
+clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+
+
+
+  /* [DONE]remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.articles a.active');
 
